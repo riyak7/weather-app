c2bd745d17f175ccb29df76e0df54f14f4013346
AN1001 <arnav.nagpure@icloud.com>
2026-05-17 18:03:42 +0100

    Proper Tab Bar Now

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
-import 'MenuButtonHolder.dart';
+import 'package:iconoir_flutter/iconoir_flutter.dart' as icons;
+
+// This is the main app, we build widgets and place them here
+// like top bar and other pages etc...
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(home: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static const List<Widget> tabs = <Widget>[
+    icons.Globe(),
+    icons.NavigatorAlt(),
+    icons.Strategy(),
+    icons.Settings(),
+  ];
+
   @override
   Widget build(BuildContext ctx){
-    return MaterialApp(
-      home: Scaffold (
+    // The whole app
+    return DefaultTabController(
+      length: tabs.length,
+
+      child: Scaffold (
+
         appBar: AppBar(
-          centerTitle: true,
-          title: MenuButtonHolder(),
+          title: const TabBar(
+            tabs: tabs,
+            dividerColor: Colors.transparent,
+            labelPadding: const EdgeInsets.only(bottom: 5.0)
+          )
         ),
+
+        body: TabBarView(
+          children: [
+            icons.Globe(),
+            icons.NavigatorAlt(),
+            icons.Strategy(),
+            icons.Settings(),
+          ],
+          
+        )
+
       )
     );
   }
 }
+//centerTitle: true, title: MenuButtonHolder()
