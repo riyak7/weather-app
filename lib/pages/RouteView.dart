699f70b0acd21e9aa6cea7bc0764d27ce1b4dde0
Harry <harryparrypoo@gmail.com>
2026-05-19 15:06:44 +0100

    Added a bit of code to import a GPX file and parse it using the gpx package. Also added the necessary dependencies to pubspec.yaml.

--- lib/pages/RouteView.dart
+++ lib/pages/RouteView.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:gpx/gpx.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:convert';
+
+//poly line layer
 
 class RouteView extends StatelessWidget {
   const RouteView({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Text("The route view should go here");
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        // Action to perform when pressed
+        await importRoute();
+      },
+      child: const Text('Import Route'),
+    );
   }
+}
+
+Future<void> importRoute() async {
+  try {
+    String fileContent = await importFile();
+    var gpxData = GpxReader().fromString(fileContent);
+    // Access waypoints (consider using a logger in production)
+    print(gpxData.wpts);
+  } catch (e) {
+    throw Exception('Failed to import file: $e');
+  }
+}
+
+Future<String> importFile() async {
+  FilePickerResult? result = await FilePicker.pickFiles(withData: true);
+  if (result == null) throw Exception('No file selected');
+  PlatformFile file = result.files.single;
+  final bytes = file.bytes;
+  if (bytes == null) return '';
+  String fileContent = utf8.decode(bytes);
+  return fileContent;
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import geolocator_apple
 import package_info_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
