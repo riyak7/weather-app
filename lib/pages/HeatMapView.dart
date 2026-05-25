e5d3be56a217b2d7505124bfb726f9d3625a6816
AN1001 <arnav.nagpure@icloud.com>
2026-05-26 00:14:50 +0100

    Heatmap now reactive when time slider moved, fixed startup bug

--- lib/pages/HeatMapView.dart
+++ lib/pages/HeatMapView.dart
@@ -50,6 +50,31 @@ class _HeatMapViewState extends State<HeatMapView> {
   // The current viewing area in LatLngs
   late LatLngBounds bounds;
 
+
+  // Add a listener to the time bar current time variable.
+  // When time is changed the arrow layer and heatmap layer get redrawn.
+  @override
+  void initState(){
+    reactiveSelectedTime.addListener(() {
+      double gridWidth = (bounds.east - bounds.west)/_gridSize;
+      double gridHeight = (bounds.north - bounds.south)/_gridSize;
+
+      _heatmapTiles.value = rawToPolygon(
+        rawData[reactiveSelectedTime.value]!,
+        gridWidth,
+        gridHeight,
+      );
+
+      _arrows.value = rawToMarker(
+        rawData[reactiveSelectedTime.value]!,
+        gridWidth,
+        gridHeight,
+      );
+    });
+  }
+
+
+
   // Clean up on deletion
   @override
   dispose() {
@@ -108,7 +133,7 @@ class _HeatMapViewState extends State<HeatMapView> {
 
                 // Converts the raw data to marker data for map
                 _arrows.value = rawToMarker(
-                  rawData.values.first,
+                  rawData[reactiveSelectedTime.value]!,
                   gridWidth,
                   gridHeight,
                 );
